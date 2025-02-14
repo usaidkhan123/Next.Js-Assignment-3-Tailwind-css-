815eea5b58a8738103a76d2a5d4b96525403a142
Muhammad Usaid <161123814+usaidkhan123@users.noreply.github.com>
2025-02-14 19:07:34 +0500

    Update about.tsx

--- app/components/Aboutus/about.tsx
+++ app/components/Aboutus/about.tsx
@@ -4,7 +4,7 @@ const Aboutus = () => {
     return (
         <div>
             <h1 className="font-serif text-8xl mt-36 ml-16 " >Boost Your <br /> <span className="text-orange-500"> Website </span> Seo </h1>
-            <p className="ml-20 font-mono	">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industry's standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
+            <p className="ml-20 font-mono	">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industrys standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
             <Image className=' ml-[55rem] -mt-96'
         src={hero}
         alt="image" 
