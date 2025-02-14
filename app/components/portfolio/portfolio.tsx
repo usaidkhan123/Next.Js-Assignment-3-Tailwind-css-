a29a2cabaa378584dbe3f22c0f806a0cc35deb35
Muhammad Usaid <161123814+usaidkhan123@users.noreply.github.com>
2025-02-14 19:09:08 +0500

    Update portfolio.tsx

--- app/components/portfolio/portfolio.tsx
+++ app/components/portfolio/portfolio.tsx
@@ -7,7 +7,7 @@ const PortFolio = () => {
             <h1 className="text-7xl mt-32 text-center font-serif py-4 mb-4">PortFolio Projects</h1>
             <h1 className="text-6xl font-serif mt-8 text-orange-500 ml-28 text-">Project:1</h1>
             <h1 className=" text-4xl ml-28 mt-8 font-semibold">E-commerce Mobile App Design</h1>
-            <p className=" ml-28 ">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industry's standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,is simply dummy <br /> text of the printing and typesetting industry. Lorem  <br />has been the industry's standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
+            <p className=" ml-28 ">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industrys standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,is simply dummy <br /> text of the printing and typesetting industry. Lorem  <br />has been the industrys standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
             <Image className=' ml-[55rem] -mt-72'
         src={hero}
         alt="image" 
@@ -16,7 +16,7 @@ const PortFolio = () => {
       />
             <h1 className="text-6xl font-serif mt-36 text-orange-500 ml-28 ">Project:2</h1>
             <h1 className=" text-4xl ml-28 mt-8 font-semibold">SocialMedia Mobile App Design</h1>
-            <p className=" ml-28 ">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industry's standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,is simply dummy <br /> text of the printing and typesetting industry. Lorem  <br />has been the industry's standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
+            <p className=" ml-28 ">is simply dummy text of the printing and typesetting industry. Lorem  <br />has been the industrys standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,is simply dummy <br /> text of the printing and typesetting industry. Lorem  <br />has been the industrys standard dummy text ever since the 1500s, when <br /> specimen book. It has survived not only five centuries,</p>
             <Image className=' ml-[55rem] -mt-72'
         src={hero2}
         alt="image" 
