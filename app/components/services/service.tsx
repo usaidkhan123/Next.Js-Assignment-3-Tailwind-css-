d2d6131e4689cfc1901f9d6f7cc634b51537412f
Muhammad Usaid <161123814+usaidkhan123@users.noreply.github.com>
2025-02-14 19:09:45 +0500

    Update service.tsx

--- app/components/services/service.tsx
+++ app/components/services/service.tsx
@@ -4,15 +4,15 @@ const Services = () => {
             <h1 className="text-6xl mt-32 text-center font-serif py-4 mb-4">Our services</h1>
             <section className="h-60 w-64  bg-white ml-36 rounded-lg justify-start ">
                 <h1 className=" font-mono text-xl font-semibold  text-center py-4 mt-12 "> Marketing</h1>
-                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industry's standard dummy  ever since the 1500s, when</p>
+                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industrys standard dummy  ever since the 1500s, when</p>
             </section>
             <section className="h-60 w-64  bg-white ml-[33rem] rounded-lg justify-start -mt-60 ">
                 <h1 className=" font-mono text-xl font-semibold  text-center py-4 "> Seo Sem</h1>
-                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industry's standard dummy  ever since the 1500s, when</p>
+                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industrys standard dummy  ever since the 1500s, when</p>
             </section>
             <section className="h-60 w-64  bg-white ml-[58rem] rounded-lg justify-start -mt-60 ">
                 <h1 className=" font-mono text-xl font-semibold  text-center py-4 "> ViralCampaign</h1>
-                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industry's standard dummy  ever since the 1500s, when</p>
+                <p className="text-center" >is simply dummy text of <br />the printing and typesetting <br /> industry. Loremhas been the   <br />industrys standard dummy  ever since the 1500s, when</p>
             </section>
         </div>
     )
